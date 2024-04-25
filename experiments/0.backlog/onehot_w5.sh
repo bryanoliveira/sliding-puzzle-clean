169e696c1a206696ab53bc68b42026e0dbb1fd26
Bryan Lincoln <bryanufg@gmail.com>
2024-04-25 16:25:06 -0300

    chore: update experiments

--- experiments/0.backlog/onehot_w5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 2x2
-python train_ray.py -c ray_ppo --seed 594 --env--w 5 --total-timesteps 10000000
-python train_ray.py -c ray_ppo --seed 267 --env--w 5 --total-timesteps 10000000
-python train_ray.py -c ray_ppo --seed 769 --env--w 5 --total-timesteps 10000000
--- experiments/1.to_run/onehot_w4.sh
+++ experiments/1.to_run/onehot_w4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for seed in 176 267 594 769 907; do
-python ppo.py --seed $seed --env_configs '{"w": 4, "variation": "onehot"}' --hidden_size 1024 --hidden_layers 5
+python ppo.py --seed $seed --env_configs '{"w": 4, "variation": "onehot"}' --hidden_size 1024 --hidden_layers 5 --total_timesteps 10000000
 done
