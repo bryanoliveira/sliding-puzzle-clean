86268713c03e32846631e3aaaf96d9ef0cdb00c6
Bryan Lincoln <bryanufg@gmail.com>
2024-04-26 11:10:10 -0300

    feat: add group name

--- ppo.py
+++ ppo.py
@@ -181,6 +181,7 @@ if __name__ == "__main__":
             sync_tensorboard=True,
             config=vars(args),
             name=run_name,
+            group=args.exp_name,
             monitor_gym=True,
             save_code=True,
         )
