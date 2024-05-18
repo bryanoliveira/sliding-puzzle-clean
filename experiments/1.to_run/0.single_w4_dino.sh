396aa42bf09709a1ef8c4b2dc020fb716bd71424
Bryan Lincoln <bryanufg@gmail.com>
2024-05-18 19:50:08 -0300

    chore: update experiments

--- experiments/1.to_run/0.single_w4_dino.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for seed in 176 267 594 769 907; do
-python ppo.py --seed $seed --env_configs '{"w": 2, "variation": "image", "image_folder": "single"}' --hidden_size 1024 --hidden_layers 5 --total_timesteps 20000000 --backbone dino
-done
-
