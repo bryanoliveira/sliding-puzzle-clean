f4499ae05683ddf1a898250353c547b79a4b2e67
Bryan Lincoln <bryanufg@gmail.com>
2024-06-20 16:19:45 -0300

    feat: improve experiment daemon

--- experiments/daemon.sh
+++ experiments/daemon.sh
@@ -9,7 +9,33 @@ if [[ -z $MAX_VRAM ]]; then
     MAX_VRAM="0.5"
 fi
 
-echo "Max RAM is $MAX_RAM and max VRAM is $MAX_VRAM"
+echo "Default max RAM is $MAX_RAM and max VRAM is $MAX_VRAM"
+
+TOTAL_RAM=$(free | awk '/Mem/{printf("%.2f"), $2/1024/1024}' | sed 's/,/\./')
+TOTAL_VRAM=$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits | head -n 1)
+TOTAL_VRAM=$(echo "scale=2; $TOTAL_VRAM / 1024" | bc)
+MIN_AVAILABLE_RAM=$(echo "scale=2; $TOTAL_RAM * $MAX_RAM" | bc)
+MIN_AVAILABLE_VRAM=$(echo "scale=2; $TOTAL_VRAM * $MAX_VRAM" | bc)
+
+calculate_ram_availability() {
+    free | awk '/Mem/{printf("%.2f"), $2/1024/1024}' | sed 's/,/\./'
+}
+
+calculate_vram_availability() {
+    nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits | head -n 1
+}
+
+move_to_executed() {
+    script=$1
+    echo "---- Moving $script to 4.executed"
+    mv "experiments/3.running/$script" "experiments/4.executed/$script"
+}
+
+move_to_failed() {
+    script=$1
+    echo "---- Moving $script to 4.failed"
+    mv "experiments/3.running/$script" "experiments/4.failed/$script"
+}
 
 while true; do
   # Check if there are any scripts in the "experiments/to_run" folder
@@ -21,36 +47,52 @@ while true; do
     mv "experiments/1.to_run/$script" "experiments/2.queued/$script"
     echo "---- Queued $script"
 
-    # Check if there is at least 50% of RAM available
-    ram_usage=$(free | awk '/Mem/{printf("%.2f"), $3/$2}' | sed 's/,/\./')
-    # Check if there is at least 50% of VRAM available
-    total_vram=$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits | head -n 1)
-    vram_usage=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | head -n 1)
+    min_available_ram=$MIN_AVAILABLE_RAM
+    min_available_vram=$MIN_AVAILABLE_VRAM
+    # Read the second line of the script to check for hardware requirements
+    req_line=$(sed -n '2p' "experiments/2.queued/$script")
+    # Check if the second line contains hardware requirements
+    if [[ $req_line == \#\ \{'requirements':* ]]; then
+        # Extract RAM and VRAM requirements from the line using jq
+        req_ram=$(echo $req_line | sed 's/^# //' | jq -r '.requirements.ram // empty')
+        req_vram=$(echo $req_line | sed 's/^# //' | jq -r '.requirements.vram // empty')
+
+        # Update min_available_ram and min_available_vram if requirements are found
+        if [[ -n $req_ram ]]; then
+            min_available_ram=$req_ram
+        fi
+        if [[ -n $req_vram ]]; then
+            min_available_vram=$req_vram
+        fi
+    fi
+
+    echo "---- Script $script requires max RAM: $max_ram and max VRAM: $max_vram"
+
+    ram_availability=$(calculate_ram_availability)
+    vram_availability=$(calculate_vram_availability)
 
     while true; do
-      if (( $(echo "$ram_usage < $MAX_RAM" | bc -l) )) && (( $(echo "($vram_usage / $total_vram) <= $MAX_VRAM" | bc -l) )); then
-        # If there is enough RAM available, break the loop and continue with the execution
+      if (( $(echo "$ram_availability > $min_available_ram" | bc -l) )) && (( $(echo "$vram_availability > $min_available_vram" | bc -l) )); then
+        # If there is enough RAM and VRAM available, break the loop and continue with the execution
         echo "---- Running $script"
         break
       else
-        # If there is not enough RAM available, print current timestamp and sleep
-        echo "---- Waiting. RAM usage: $ram_usage% / VRAM usage: $vram_usage MB"
+        # If there is not enough RAM or VRAM available, print current timestamp and sleep
+        echo "---- Waiting. Available RAM: $ram_availability GB / Available VRAM: $vram_availability GB / Required RAM: $min_available_ram GB / Required VRAM: $min_available_vram GB"
         date
         sleep 180
 
-        ram_usage=$(free | awk '/Mem/{printf("%.2f"), $3/$2}' | sed 's/,/\./')
-        vram_usage=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | head -n 1)
+        ram_availability=$(calculate_ram_availability)
+        vram_availability=$(calculate_vram_availability)
       fi
     done
 
     echo "---- Moving $script to 3.running"
     mv "experiments/2.queued/$script" "experiments/3.running/$script"
 
-    # Execute the script
-    source "experiments/3.running/$script"
-
-    echo "---- Moving $script to 4.executed"
-    mv "experiments/3.running/$script" "experiments/4.executed/$script"
+    # Execute the script in the background and move it to the appropriate folder based on the exit status
+    (source "experiments/3.running/$script" && move_to_executed "$script" || move_to_failed "$script") &
+    sleep 30 # time to wait for the training begin using resources
   else
     # If there are no scripts in the "experiments/1.to_run" folder, print current timestamp and sleep
     echo "---- No scripts to run"
